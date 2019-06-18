ba5b2a21b203a74ebf5961bde0021110aae800c3
Ming Liu <mliu@macpro.home>
2019-06-18 18:46:09 -0400

    add state tracking for outs and innings

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,11 @@ const App = props =>  {
 
   const [state, setState] = useState({
     balls : 0,
-    strikes : 0
+    strikes : 0,
+    inning : 1,  // up to 18, odd -> top, even -> bottom, Math.ceil(division) to number of innings
+    out: 0   //up to 3 then switching to top/bottom inning
+    
+
   })
 
   const recordBalls = event => {  
@@ -27,21 +31,39 @@ const App = props =>  {
   const recordStrikes = event => {
     event.preventDefault();
     const newCount = state.strikes + 1
-    if(newCount >= 3) {
-      setState({ ...state, balls: 0, strikes: 0})
+    if(newCount >= 3) {  //strike out
+      const newOut = state.out + 1
+      console.log("You are out!")
+      if(newOut >= 3) {  //end of half inning, switch between top/bottom
+        const newInning = state.inning + 1;
+        setState({ ...state, balls: 0, strikes: 0, out: 0, inning: newInning})
+        console.log("THREE OUTS!!! The end of half inning!")
+        console.log(`At ${state.inning % 2 === 0 ? "bottom" : "top"} of the ${Math.ceil(state.inning/2)} innings, ${state.out} outs`)
+
+      } else {
+        setState({ ...state, balls: 0, strikes: 0, out: newOut})
+        console.log(`At ${state.inning % 2 === 0 ? "bottom" : "top"} of the ${Math.ceil(state.inning/2)} innings, ${state.out} outs`)
+
+      }
     } else {
       setState({ ...state, strikes: newCount });
+      console.log(`At ${state.inning % 2 === 0 ? "bottom" : "top"} of the ${Math.ceil(state.inning/2)} innings, ${state.out} outs`)
+
     }  
   }
 
   const recordFoul = event => {
+    event.preventDefault();
     if(state.strikes < 2) {
       const newCount = state.strikes + 1
       setState({ ...state, strikes: newCount})
+      console.log(`At ${state.inning % 2 === 0 ? "bottom" : "top"} of the ${Math.ceil(state.inning/2)} innings, ${state.out} outs`)
+
     }
   }
 
   const recordHit = event => {
+    event.preventDefault();
     setState({...state, balls : 0, strikes : 0})
   }
 
